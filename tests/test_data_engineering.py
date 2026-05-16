a95fc5c4e1a39be5102967847f4c400e7ea046d1
Amilcar Campos <contact@amicampos.com>
2026-05-16 00:22:46 -0600

    eee

--- tests/test_data_engineering.py
+++ tests/test_data_engineering.py
@@ -1,18 +1,6 @@
 # ruff: noqa
 import pytest
 
-# Importamos las constantes desde tu módulo de utilidades
-from data_engineering.utils.utils import WHITELIST_COLUMNS, IMPUTATION_COLUMNS
-
-
-@pytest.mark.ci_exclude
-def test_utils_constants_are_not_empty():
-    """Verifica que las listas de configuración de columnas se importen correctamente."""
-    # Comprobamos que existan y sean listas/diccionarios con datos
-    assert WHITELIST_COLUMNS is not None
-    assert len(WHITELIST_COLUMNS) > 0
-    assert isinstance(IMPUTATION_COLUMNS, dict)
-
 
 def test_ejemplo_simple():
     """Una prueba matemática básica para asegurar que pytest responda."""
