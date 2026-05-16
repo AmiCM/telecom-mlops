1a22b3a8cbe6c6c1d95cfbf9d0e05a7daf1fb75c
Amilcar Campos <contact@amicampos.com>
2026-05-16 00:11:45 -0600

    refactor

--- tests/test_data_engineering.py
+++ tests/test_data_engineering.py
@@ -4,7 +4,7 @@ import pytest
 # Importamos las constantes desde tu módulo de utilidades
 from data_engineering.utils.utils import WHITELIST_COLUMNS, IMPUTATION_COLUMNS
 
-
+@pytest.mark.ci_exclude
 def test_utils_constants_are_not_empty():
     """Verifica que las listas de configuración de columnas se importen correctamente."""
     # Comprobamos que existan y sean listas/diccionarios con datos
